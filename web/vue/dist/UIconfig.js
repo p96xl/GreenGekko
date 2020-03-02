4ad97d0d6edad3d30032b78d92b73ba80fbbfc32
p96xl <p96xlyuri@gmail.com>
2020-03-02 00:06:33 -0600

    Update UIconfig.js

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -5,7 +5,8 @@
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
 const CONFIG = {
-  headless: false,
+  headless: true,
+// it gets annoying
   api: {
     host: '127.0.0.1',
     port: 3000,
